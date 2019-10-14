530315a1b73fd1ee80dc9849fba2d610585ad743
Fonov_Aleksei <wakeup77794@gmail.com>
2019-10-15 01:59:06 +0300

    minor changes has been done, step two. All is well

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView, View } from 'react-native'
+import { ScrollView, View, } from 'react-native'
 import { Header, InformRow, TopRow } from './src/components/uikit'
 import { url } from './constants'
 
@@ -30,10 +30,10 @@ export default class App extends Component {
 
 		const { title, data } = this.state
 		return (
-			<View>
+			<View style={{ backgroundColor: '#c1ecf4'  }}>
 				<Header title={title}/>
 				<TopRow/>
-				<ScrollView style={{ backgroundColor: '#c1ecf4' }}>
+				<ScrollView >
 					<View style={{ marginBottom: 150 }}>
 						{data.map(item => (
 							<InformRow data={item} key={item.name}/>
